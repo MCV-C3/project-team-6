963806dde4e2a964948626295a36b3ac4df71b14
Aleixrei73 <aleix.armero73@gmail.com>
2026-01-10 20:55:56 +0100

    Config error

--- Week3/sweeplow.sh
+++ Week3/sweeplow.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH -n 8 # Number of cores
 #SBATCH -p mlow # Partition to submit to
-#SBATCH --array=1-50
+#SBATCH --array=1-200
 #SBATCH --mem 24G # 24GB memory
 #SBATCH --gres gpu:1 # Request of 1 gpu
 #SBATCH -o logs/%x_%u_%j.out # File to which STDOUT will be written
 #SBATCH -e logs/%x_%u_%j.err # File to which STDERR will be written
 
-wandb agent --count 1 mcv-team-6/C3-Week3/8yeenlu8
+wandb agent --count 1 mcv-team-6/C3-Week3/ig5z7cnx
