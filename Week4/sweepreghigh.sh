cc378507c2d3c6b9cb4b6669ec7d8b5d9db695a1
Adri <adriarp03@gmail.com>
2026-01-23 11:24:21 +0100

    created sh

---
+++ Week4/sweepreghigh.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH -p mhigh # Partition to submit to
+#SBATCH --mem 24G # 24GB memory
+#SBATCH --gres gpu:1 # Request of 1 gpu
+#SBATCH -o logs/%x_%u_%j.out # File to which STDOUT will be written
+#SBATCH -e logs/%x_%u_%j.err # File to which STDERR will be written
+
+wandb agent mcv-team-6/C3-Week4/188jlsn5
