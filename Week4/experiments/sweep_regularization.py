39e179d9e3afc3277c4a95c77986aa5db3f68143
Adri <adriarp03@gmail.com>
2026-01-23 11:43:31 +0100

    small correction

--- Week4/experiments/sweep_regularization.py
+++ Week4/experiments/sweep_regularization.py
@@ -4,25 +4,32 @@ import utils
 import torch
 import torch.nn as nn
 from pytorch_lightning.loggers import WandbLogger
+import pytorch_lightning as pl
 import kornia.augmentation as ka
 import wandb
 
+
 argparser = utils.get_experiment_argument_parser()
 args, unknown = argparser.parse_known_args()
-wandb.init()
-config = wandb.config
+sweep_config = {}
+for arg in unknown:
+    if arg.startswith("--"):
+        key, val = arg[2:].split("=")
+        try:
+            val = float(val)
+            if val.is_integer():
+                val = int(val)
+        except ValueError:
+            pass
+        sweep_config[key] = val
 
-start_layer = 0 if config.dropout_loc == "all" else 3
-
-model = VariableGenericSE(
-    channels_list=[16, 24, 24, 24, 32, 32, 32, 32], 
-    use_residuals=config.use_residuals,
-    use_cbam=config.use_cbam,
-    dropout_rate=config.dropout_val,
-    dropout_start_layer=start_layer
-)
-
+EPOCHS = args.epochs
 IMG_SIZE = 224
+LR = 0.001
+
+dry = args.dry
+device = utils.set_device(args.gpu_id)
+
 torch.manual_seed(0)
 torch.cuda.manual_seed(0)
 
@@ -44,29 +51,49 @@ augmentation = ka.AugmentationSequential(
         ka.Resize(size=(IMG_SIZE, IMG_SIZE))
     )
 
-loss_fn = nn.CrossEntropyLoss(label_smoothing=0.1)
+wandb_logger = WandbLogger(
+    project="C3-Week4",
+    entity="mcv-team-6",
+    name="Reg Search"
+)
 
-train_model = WeightDecayTrainer(
-    model=model, 
-    loss_fn=loss_fn,
-    augmentations=augmentation, 
-    lr=0.001,
-    weight_decay=config.weight_decay
+wandb_logger.experiment.config.update(sweep_config)
+cfg = wandb_logger.experiment.config
+
+
+start_layer = 0 if cfg.get('dropout_loc', 'mid') == "all" else 3
+
+model = VariableGenericSE(
+    channels_list=[16, 24, 24, 24, 32, 32, 32, 32], 
+    use_residuals=cfg.get('use_residuals', False),
+    use_cbam=cfg.get('use_cbam', False),
+    dropout_rate=cfg.get('dropout_val', 0.0),
+    dropout_start_layer=start_layer
 )
 
 
-wandb_logger = WandbLogger(project="C3-Week4", entity="mcv-team-6")
-wandb_logger.experiment.config.update({
-    "parameters": sum(p.numel() for p in model.parameters()),
-    "architecture": "GenericSE_Reg_Search"
-})
+total_params = sum(p.numel() for p in model.parameters())
+sweep_config["parameters"] = total_params
+wandb_logger.experiment.config.update(sweep_config)
 
-trainer = utils.get_trainer(wandb_logger, patience=30, min_delta=0.001, epochs=args.epochs)
+
+
+train_model = WeightDecayTrainer(model=model, augmentations=augmentation, lr=LR, weight_decay=cfg.get('weight_decay', 0.0))
+
+
+
+
+trainer = utils.get_trainer(wandb_logger, patience=30, min_delta=0.001, epochs=EPOCHS)
+
 train_loader, test_loader = utils.get_loaders(
     image_size=(IMG_SIZE, IMG_SIZE),
-    resize_train=True, resize_test=True,
+    resize_train=True,
+    resize_test=True,
     train_batch_size=64,
-    train_folder=args.train_folder, test_folder=args.test_folder
+    train_folder=args.train_folder,
+    test_folder=args.test_folder,
 )
 
 trainer.fit(train_model, train_loader, test_loader)
+
+wandb_logger.experiment.finish()
